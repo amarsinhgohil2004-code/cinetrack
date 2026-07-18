9ae9dc641468d5d9d16f7e5471283fbef3604191
Amarsinh Gohil <amarsinhgohil2004@gmail.com>
2026-07-18 18:46:20 +0530

    Update favicon

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -9,7 +9,7 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
-
+console.log("API URL:", import.meta.env.VITE_API_URL);
 // ===============================
 // Add JWT Token Automatically
 // ===============================
