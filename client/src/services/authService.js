d73106d2d29a7eddb5d674cd9ffb4696e983fec8
Amarsinh Gohil <amarsinhgohil2004@gmail.com>
2026-07-18 19:08:51 +0530

    error solve

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,11 +1,29 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api/auth",
+    baseURL: `${import.meta.env.VITE_API_URL}/auth`,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
-export const registerUser = (userData) =>
-    API.post("/register", userData);
+// Add token automatically
+API.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
 
-export const loginUser = (userData) =>
-    API.post("/login", userData);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
+
+export const registerUser = (userData) => {
+    return API.post("/register", userData);
+};
+
+export const loginUser = (userData) => {
+    return API.post("/login", userData);
+};
+
+export default API;
